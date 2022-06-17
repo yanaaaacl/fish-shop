3e868bf42e4be5b75e69b47a41e01896b4c2e297
yanaaaacl <a9506862137@yandex.ru>
2022-06-17 15:52:50 +0300

    slick

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
 $(function(){
 
+    $(".header-slider").slick({
+        dots: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="images/arrowup.svg" alt="arrow"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="images/arrowdone1.svg" alt="arrow"></button>',
+        vertical: true
+
+
+    });
+
 });
